5e286fc3041d067045ab9a387a082366969603c7
duda973 <duda.todorovic.97@gmail.com>
2018-03-23 18:52:17 +0100

    Napravljena klasa Menjacnica
    Implementira interfejs menjacnicainterface, dodate su metode iz
    interfejsa, bez tela.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.menjacnicainterface.MenjacnicaInterface;
+
+public class Menjacnica implements MenjacnicaInterface{
+
+	@Override
+	public void dodajKursValute(Valuta v, Kurs k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKursNaDan(Valuta v, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs vratiKursNaDan(Valuta v, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
