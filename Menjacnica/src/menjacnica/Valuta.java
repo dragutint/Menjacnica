68f68dd5448fce348a5af1e00de193b3df2eabb2
duda973 <duda.todorovic.97@gmail.com>
2018-03-23 18:37:01 +0100

    Hashcode & Equals metode
    Dodate metode za obe klase. Kod klase Valuta, metoda equals proverava
    samo naziv valute.

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -31,4 +31,32 @@ public class Valuta {
 		this.kursevi = kursevi;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kursevi == null) ? 0 : kursevi.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skracenica == null) ? 0 : skracenica.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		
+		if(obj == null) {
+			return false;
+		}
+		
+		Valuta v = (Valuta) obj;
+		if(v.getNaziv().equals(this.getNaziv())) {
+			return true;
+		}
+		
+		return false;
+	}
+
 }
